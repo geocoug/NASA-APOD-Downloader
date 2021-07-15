04276bd850ede3c181abf9238240775ee8c94e22
geocoug <grantcaleb22@gmail.com>
2021-07-15 14:17:27 -0700

    init

---
+++ nasa_apod_download.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+# NASA APOD Downloader
+# https://github.com/geocoug/NASA-APOD-Downloader.git
+
+import os
+import sys
+import requests
+import json
+import datetime
+
+
+if os.name == 'nt':
+    fpath = "D:/Pictures/Wallpaper"
+else:
+    fpath = "/mnt/d/Pictures/Wallpaper"
+
+with open('../../nasa.txt', 'r') as f:
+    API_KEY = f.readlines()[0]
+
+apod = "https://api.nasa.gov/planetary/apod?api_key={}".format(API_KEY)
+
+def GetRequest(url):
+    response = requests.get(url)
+    if not response.ok:
+        print(response.text)
+        sys.exit()
+    else:
+        return response
+
+def GetAPOD():
+    response = GetRequest(apod)
+
+    if response.json()['media_type'] == 'image':
+        if 'hdurl' in response.json():
+            img_url = response.json()['hdurl']
+        elif 'url' in response.json():
+            img_url = response.json()['url']
+    else:
+        sys.exit()
+    return img_url
+
+def SaveImg(url):
+    img_name = url.split("/")[-1]
+    img_data = GetRequest(url).content
+    with open(os.path.join(fpath, img_name), 'wb') as handler:
+        handler.write(img_data)
+
+if __name__ == "__main__":
+    SaveImg(GetAPOD())
+
