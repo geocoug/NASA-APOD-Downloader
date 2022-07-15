3a4964932636128cefc5941464de225ba405b30b
Caleb Grant <geocoug@gmail.com>
2022-07-15 09:26:47 -0700

    + command line arguments, pre-commit, rm docker

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
 # nasa_apod_dl
 
-[![Docker](https://github.com/geocoug/nasa-apod-dl/workflows/docker%20build/badge.svg)](https://github.com/geocoug/nasa-apod-dl/actions)
 [![GitHub Super-Linter](https://github.com/geocoug/nasa-apod-dl/workflows/lint%20code%20base/badge.svg)](https://github.com/marketplace/actions/super-linter)
 [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 ![license](https://img.shields.io/github/license/geocoug/nasa-apod-dl)
 
-Download the NASA Astronomy Picture of the Day (APOD).
+Download the 🚀 NASA 🚀 Astronomy Picture of the Day (APOD).
 
 ## Usage
 
-1. Set the `fpath` variable to the path where to save APOD download.
+1. Set the `FPATH` variable to the path where to save APOD download.
 
-1. Set the `api_key_file` variable to the file path containing your NASA API key.
+1. Set the `API_KEY_FILE` variable to the file path containing your NASA API key.
 
-1. Run the script:
+### Using a Python virtual environment
 
-`python3 nasa_apod_dl.py`
+1. Create Python venv: `python3 -m venv venv && source ./venv/bin/activate`
 
-**Or**
+1. Install dependencies: `python -m pip install -r requirements.txt`
 
-`docker build -t nasa-apod-dl .`
-
-`docker run --rm -it -v $(PWD)/nasa.txt:/usr/src/app/nasa.txt nasa-apod-dl`
+1. Run the script: `python nasa_apod_dl.py`
--- nasa_apod_dl.py
+++ nasa_apod_dl.py
@@ -4,52 +4,104 @@
 # https://github.com/geocoug/NASA-APOD-Downloader.git
 
 
+import argparse
+import logging
 import os
 
 import requests
 
-# Local path to save APOD download
-fpath = "/Users/cgrant/Pictures/Wallpaper"
-
-# File containing NASA API key
-api_key_file = "/Users/cgrant/OneDrive/GitHub/nasa.txt"
-
-with open(api_key_file, "r") as f:
-    API_KEY = f.readlines()[0]
-
-# API endpoint
-apod = "https://api.nasa.gov/planetary/apod?api_key={}".format(API_KEY)
+logging.basicConfig(level=logging.INFO, format="%(msg)s")
+log = logging.getLogger(__name__)
+log.propagate = False
 
 
-def GetRequest(url: str):
-    """Send a GET request"""
-    response = requests.get(url)
+def clparser() -> argparse.ArgumentParser:
+    """Create a parser to handle input arguments and displaying.
+
+    a script specific help message.
+    """
+    desc_msg = """Download the NASA Astronomy Picture of the Day (APOD)."""
+    parser = argparse.ArgumentParser(
+        description=desc_msg,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "api_token",
+        help="""NASA API token. Either a filename containing the token or the raw string.""",
+    )
+    parser.add_argument("img_path", help="""Path to store the downloaded image file.""")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="""Control the amount of verbosity.""",
+    )
+    return parser
+
+
+def get_request(request: str):
+    """Send a GET request."""
+    response = requests.get(request)
     if not response.ok:
         raise Exception(response)
     return response
 
 
-def GetAPOD() -> str:
-    """Retrieve POD url from payload"""
-    response = GetRequest(apod)
+def get_apod(request: str):
+    """Retrieve POD url from payload."""
+    response = get_request(request)
 
     if response.json()["media_type"] == "image":
         if "hdurl" in response.json():
             img_url = response.json()["hdurl"]
+            log.info("Retrieved HD image URL: %s", img_url)
         elif "url" in response.json():
             img_url = response.json()["url"]
+            log.info("Retrieved image URL: %s", img_url)
+        else:
+            img_url = None
+            log.info("No image URL retrieved.")
     else:
         raise Exception("POD not in image format.")
     return img_url
 
 
-def SaveImg(url: str) -> None:
+def save_img(response: str, save_dir: str) -> None:
     """Retrieve the iamge and save it locally."""
-    img_name = url.split("/")[-1]
-    img_data = GetRequest(url).content
-    with open(os.path.join(fpath, img_name), "wb") as handler:
+    img_name = response.split("/")[-1]
+    img_data = get_request(response).content
+    if os.path.isdir(save_dir):
+        save_path = os.path.join(save_dir, img_name)
+    else:
+        raise Exception(f"Save directory does not exist: {save_dir}")
+    log.info("Saving image at: %s", save_path)
+    with open(save_path, "wb") as handler:
         handler.write(img_data)
 
 
+def get_token(parameter: str) -> str:
+    """Return a token string."""
+    if os.path.exists(parameter):
+        try:
+            with open(parameter, "r", encoding="utf-8") as file:
+                _token = file.readlines()[0]
+        except Exception as error:
+            raise error
+    else:
+        _token = parameter
+    return _token
+
+
 if __name__ == "__main__":
-    SaveImg(GetAPOD())
+    args = clparser().parse_args()
+    api_token = args.api_token
+    img_path = args.img_path
+    verbose = args.verbose
+    if verbose:
+        log.addHandler(logging.StreamHandler())
+
+    token = get_token(api_token)
+    endpoint = f"https://api.nasa.gov/planetary/apod?api_key={token}"
+    url = get_apod(endpoint)
+    if url:
+        save_img(url, img_path)
